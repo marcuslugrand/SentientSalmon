b28cb6ef31c0f1e89e19435a432caaed2601570e
Noah Gorgevski-Sharpe <ngsharpe13@gmail.com>
2023-03-27 23:15:54 -0400

    Added lunge mechanic. needs tuning

--- Assets/Scripts/Minigame Scripts/PlayerBearController.cs
+++ Assets/Scripts/Minigame Scripts/PlayerBearController.cs
@@ -7,6 +7,14 @@ public class PlayerBearController : MonoBehaviour
     // Movement\
     [SerializeField]
     private float moveSpeed;
+    [SerializeField]
+    private float lungeLength;
+    [SerializeField]
+    private float lungeCooldown;
+    [SerializeField]
+    private float lungeSpeed;
+    private float lungeTimer = 0f;
+    private float lungeCooldownTimer = 0f;
     private int horizontal;
     private int vertical;
 
@@ -26,13 +34,31 @@ public class PlayerBearController : MonoBehaviour
         horizontal = (int) Input.GetAxisRaw("Horizontal");
         vertical = (int) Input.GetAxisRaw("Vertical");
 
-        anim.SetInteger("SpeedX", horizontal);
-        anim.SetInteger("SpeedY", vertical);
+        anim.SetInteger("SpeedX", 0);
+        anim.SetInteger("SpeedY", 0);
 
-        rb.velocity = new Vector2(horizontal, vertical).normalized * moveSpeed;
+        if (lungeCooldownTimer < 0){
+            anim.SetInteger("SpeedX", horizontal);
+            anim.SetInteger("SpeedY", vertical);
+
+            rb.velocity = new Vector2(horizontal, vertical).normalized * moveSpeed;
+        }
+
+        if (lungeTimer < 0 && lungeCooldownTimer > 0){
+            rb.velocity = Vector2.zero;
+        }
+
+        lungeTimer -= Time.deltaTime;
+        lungeCooldownTimer -= Time.deltaTime;
+
+        if (lungeCooldownTimer < 0 && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse1))){
+            rb.velocity = new Vector2(horizontal, vertical) * lungeSpeed;
+            lungeCooldownTimer = lungeCooldown;
+            lungeTimer = lungeLength;
+        }
     }
 
-    private void OnCollisionEnter2D(Collision2D other) {
+    private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "AI Salmon"){
             Destroy(other.gameObject);
         }
