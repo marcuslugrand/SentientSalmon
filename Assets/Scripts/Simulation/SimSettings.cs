1384b113247ad9c4d2b4ab9475e2d9a135829fb6
connergg <cgallimore97@ufl.edu>
2023-04-04 10:17:19 -0400

    slider values now display as text

--- Assets/Scripts/Simulation/SimSettings.cs
+++ Assets/Scripts/Simulation/SimSettings.cs
@@ -12,16 +12,28 @@ public class SimSettings : MonoBehaviour
     public Slider bearAggressionSlider;
     public Slider currentResistanceSlider;
     public TMP_Dropdown mapSelection;
+    public TextMeshProUGUI popCountValueText;
+    public TextMeshProUGUI salmonMaxSpeedValueText;
+    public TextMeshProUGUI bearSpeedValueText;
+    public TextMeshProUGUI bearAggressionValueText;
+    public TextMeshProUGUI currentResistanceValueText;
     public string[] mapNames = {"Autumn Map", "Spring Map", "Winter Map"};
     void Start()
     {
        // add listeners to sliders; invoke methods when value of slider changes 
-       popCountSlider.onValueChanged.AddListener(delegate {ChangeSliderValue(popCountSlider);});
-       salmonMaxSpeedSlider.onValueChanged.AddListener(delegate {ChangeSliderValue(salmonMaxSpeedSlider);});
-       bearSpeedSlider.onValueChanged.AddListener(delegate {ChangeSliderValue(bearSpeedSlider);});
-       bearAggressionSlider.onValueChanged.AddListener(delegate {ChangeSliderValue(bearAggressionSlider);});
-       currentResistanceSlider.onValueChanged.AddListener(delegate {ChangeSliderValue(currentResistanceSlider);});
+       popCountSlider.onValueChanged.AddListener(delegate {ChangeSliderValue(popCountSlider, popCountValueText);});
+       salmonMaxSpeedSlider.onValueChanged.AddListener(delegate {ChangeSliderValue(salmonMaxSpeedSlider, salmonMaxSpeedValueText);});
+       bearSpeedSlider.onValueChanged.AddListener(delegate {ChangeSliderValue(bearSpeedSlider, bearSpeedValueText);});
+       bearAggressionSlider.onValueChanged.AddListener(delegate {ChangeSliderValue(bearAggressionSlider, bearAggressionValueText);});
+       currentResistanceSlider.onValueChanged.AddListener(delegate {ChangeSliderValue(currentResistanceSlider, currentResistanceValueText);});
        mapSelection.onValueChanged.AddListener(delegate {ChangeDropdownValue(mapSelection);});
+
+       // display default slider values...can be modified in Unity Editor
+       ChangeSliderValue(popCountSlider, popCountValueText);
+       ChangeSliderValue(salmonMaxSpeedSlider, salmonMaxSpeedValueText);
+       ChangeSliderValue(bearSpeedSlider, bearSpeedValueText);
+       ChangeSliderValue(bearAggressionSlider, bearAggressionValueText);
+       ChangeSliderValue(currentResistanceSlider, currentResistanceValueText);
     }
 
     // Update is called once per frame
@@ -30,9 +42,10 @@ public class SimSettings : MonoBehaviour
         
     }
 
-    public void ChangeSliderValue(Slider slider)
+    public void ChangeSliderValue(Slider slider, TextMeshProUGUI valueText)
     {
         Debug.Log((int)slider.value);
+        valueText.text = ((int)slider.value).ToString();
     }
 
     public void ChangeDropdownValue(TMP_Dropdown dropdown)
